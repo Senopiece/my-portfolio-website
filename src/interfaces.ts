21b2a44bf258440920c11b3b5f13b604cbf09d7b
Senopiece <shichiromail@gmail.com>
2023-07-02 05:29:43 +0300

    convert comic to ts

---
+++ src/interfaces.ts
@@ -0,0 +1,12 @@
+export interface ComicResponse {
+    img: string;
+    alt: string;
+    safe_title: string;
+    year: string;
+    month: string;
+    day: string;
+}
+
+export interface Hw2Response {
+    id: string;
+}
